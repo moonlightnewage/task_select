0b6879ef2c3052a20c17f76df6d7a23ea496219b
Olha Yevtushenko <olha.yevtushenko@outlook.com>
2017-01-04 12:24:42 +0200

    no message

--- js/main.js
+++ js/main.js
@@ -6,45 +6,39 @@ $(document).ready(function() {
         
     $(selects).each(function() {
         var self = $(this);
-        
-        // Placeholder BEGIN
-        $(self).change(function() {
-            if ($(self).val() == '0') {
-                $(self).addClass('empty');
-            }
-            else {
-                $(self).removeClass('empty');
-            }
-        }).change();
-        // Placeholder END
-        
-        $(self).wrap('<div class="select__wrapper"></div>').after('<ul class="select__dropdown"></ul>');
+        $(self).hide();
+               
+        $(self).wrap('<div class="select__wrapper"></div>').before('<div class="select__placeholder">Город *</div>').after('<ul class="select__dropdown"></ul>');
         options = $(self).children();
-        $(options).hide();
         dropdown = $(self).next('ul.select__dropdown')
-        $(dropdown).hide();
-        for (var i = 1; i < options.length; i++) {
+        $('ul.select__dropdown').addClass('hidden');
+        for (var i = 0; i < options.length; i++) {
                 $('<li data-item="' + i + '"></li>').text(options[i].innerHTML).appendTo(dropdown); 
             }
         
         // Clickers BEGIN
-        $(self).on('click', function(e) {
+        $(self).prev().on('click', function(e) {
+            e.stopPropagation();
             var target = $(e.target);
-            $(target).next(dropdown).toggle();
+            $(target).parent().children().last().toggleClass('hidden');
+            
         });
         
         $(dropdown).children().on('click', function(e) {
+            e.stopPropagation();
             var target = $(e.target);
             $(target).addClass('active').siblings().removeClass('active');
             value = $(target).attr('data-item');
             $(self).find('option[value="' + value + '"]').attr('selected', 'selected').siblings().removeAttr('selected');
-            $(dropdown).hide();
+            $(self).prev().text($(target).text());
+            $(target).parent().addClass('hidden');
         });
         
         $(document).on('click', function(e) {
+            e.stopPropagation();
             var target = $(e.target);
-            if (!$(target).is('select')) {
-               $('ul.select__dropdown').hide();
+            if (!$(target).is(self)) {
+               $('ul.select__dropdown').addClass('hidden');
             }
         });
         // Clickers END
